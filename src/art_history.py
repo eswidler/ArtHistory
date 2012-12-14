61970dd2d125a6d36c4501fb6993600775057603
Stephanie Gary <scg64@cornell.edu>
2012-12-13 20:26:48 -0500

    merged

--- src/art_history.py
+++ src/art_history.py
@@ -128,7 +128,7 @@ class PaintingHandler(BaseHandler):
     SUPPORTED_METHODS = ("PUT", "GET", "DELETE","POST")
     
     def get(self, paintingID, format):
-        painting= self.db.get_painting(paintingID)
+        painting= self.db.get_painting(paintingID,self.base_uri)
         mappings= {".html":"text/html",".xml":"application/xml",".ttl":"text/turtle"}
         painting['success']=True
         
@@ -162,7 +162,7 @@ class PaintingHandler(BaseHandler):
             self.write_error(404,message="Painting %s does not exist" %paintingID)
             
     def delete(self, paintingID, format):
-        if paintingID in self.db.movies:
+        if paintingID in self.db.paintings:
             print "Deleting painting %s" % paintingID
             self.db.delete_painting(paintingID)
         else:
@@ -253,9 +253,11 @@ class PaintingDatabase(object):
         return paintings
     
     
-    def get_painting(self, paintingID):
+    def get_painting(self, paintingID,base_uri):
         """Returns data about a painting"""
         painting= self.paintings[paintingID]
+        uri= base_uri +"/paintings/"+str(paintingID)
+        painting["uri"]=uri
         return painting
     
     def update_painting(self, paintingID, painting):
